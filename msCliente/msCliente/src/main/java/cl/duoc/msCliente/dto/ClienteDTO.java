0d3b6c44fe08b6600987ab7c50dffeab7d246dc4
bnmnnm <be.curinanco@duocuc.cl>
2026-05-17 08:27:51 -0400

    Add files via upload

---
+++ msCliente/msCliente/src/main/java/cl/duoc/msCliente/dto/ClienteDTO.java
@@ -0,0 +1,14 @@
+package cl.duoc.msCliente.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClienteDTO {
+    private Integer id;
+    private String nombre;
+    private String rut;
+}
